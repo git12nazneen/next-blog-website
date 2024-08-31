4ad08784a8b78d3a33d1a45c3edb87b7a5d597e0
git12nazneen <lipinazneen65@gmail.com>
2024-08-31 13:32:22 +0600

    Added folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 // write rfc
-
-import React from 'react'
-
+import Featured from '@/components/featured/Featured'
+import CategoryList from '@/components/categorylist/CategoryList'
+import CardList from '@/components/cardlist/CardList'
+import Menu from '@/components/menu/Menu'
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Featured></Featured>
+      <CategoryList></CategoryList>
+      <div>
+        <CardList></CardList>
+        <Menu></Menu>
+      </div>
+    </div>
   )
 }
---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Pagination from '@/components/pagination/Pagination'
+export default function Menu() {
+  return (
+    <div>
+      <Pagination></Pagination>
+    </div>
+  )
+}
