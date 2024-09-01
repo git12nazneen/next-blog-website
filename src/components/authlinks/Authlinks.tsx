66a76785c90aa6e74504ac623699d860bf0234ac
git12nazneen <lipinazneen65@gmail.com>
2024-09-01 17:37:12 +0600

    added react quil

--- src/components/authlinks/Authlinks.tsx
+++ src/components/authlinks/Authlinks.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import React from 'react';
 
 export default function Authlinks() {
-  const status = 'notauthentication';  // Match this value with the condition
+  const status = 'authentication';  // Match this value with the condition
 
   return (
     <div className='text-gray-300'>
@@ -13,7 +13,7 @@ export default function Authlinks() {
       ) : (
         <div>
           <Link href="/write">Write</Link>
-          <span className=''>Logout</span>
+          <span className='mx-5'>Logout</span>
         </div>
       )}
     </div>
