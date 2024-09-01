ae628727071d5401b5c95e223a78d71e6cb86fae
git12nazneen <lipinazneen65@gmail.com>
2024-09-01 14:24:25 +0600

    Added id

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 export default function Blog() {
   return (
     <div>
-      <div className='bg-orange-600 py-6 mx-auto'><h1 className='text-white text-center font-bold text-3xl'>The blog</h1></div>
+      <div className='bg-orange-600 py-6 mx-auto'><h1 className='text-white text-center font-bold text-3xl'>Style blog</h1></div>
       <div className="flex gap-5 py-20 px-10">
           <div className="w-2/3">
             {" "}
--- src/components/cardlist/CardList.jsx
+++ src/components/cardlist/CardList.jsx
@@ -1,16 +1,14 @@
-
-import Card from '../card/Card'
-import Pagination from '../pagination/Pagination'
+import Card from "../card/Card";
+import Pagination from "../pagination/Pagination";
 export default function CardList() {
   return (
-    <><Card></Card>
-    <Card></Card>
-    <Card></Card>
-    <Card></Card>
-
-
+    <>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
       {/* pagination */}
       <Pagination></Pagination>
     </>
-  )
+  );
 }
